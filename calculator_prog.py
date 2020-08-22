c4cd51aca50c8ac4098a68c2ae5d0dcfa5ab0bbd
Killdoz <70052201+Killdoz@users.noreply.github.com>
2020-08-22 18:31:00 +0300

    calculator

--- calculator_prog.py
+++ calculator_prog.py
@@ -3,16 +3,16 @@ print("Hi!")
 print("choose your first number")
 number = int(input())
 print("choose your symbol")
-znak = input()
+symbol = input()
 print("choose your second number")
 sec_number = int(input())
-if (znak == '+'):
+if (symbol == '+'):
     print(number + sec_number)
-if (znak == "-"):
+if (symbol == "-"):
     print(number - sec_number)
-if (znak == "*"):
+if (symbol == "*"):
     print(number * sec_number)
-if (znak == "/"):
+if (symbol == "/"):
     print(number / sec_number)
 print("thank you to use this calculator :)")
 time.sleep(5)
